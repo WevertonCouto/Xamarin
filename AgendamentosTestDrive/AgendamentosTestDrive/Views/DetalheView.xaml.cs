36991ae7d15e93705fcae3b033d7124cd1a2fba4
Weverton Couto <wevertoncristais@gmail.com>
2020-01-18 21:17:01 -0300

    Binding Text and SwitchCell

--- AgendamentosTestDrive/AgendamentosTestDrive/Views/DetalheView.xaml.cs
+++ AgendamentosTestDrive/AgendamentosTestDrive/Views/DetalheView.xaml.cs
@@ -12,7 +12,32 @@ namespace AgendamentosTestDrive.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalheView : ContentPage
     {
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
         private Veiculo veiculo;
+
+        public string TextoFreioAbs { get {
+                return string.Format("Freio ABS - R$: {0}", FREIO_ABS) ;
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$: {0}", AR_CONDICIONADO);
+            }
+        }
+
+        public string MP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$: {0}", MP3_PLAYER);
+            }
+        }
+
         public DetalheView(Veiculo veiculo)
         {
             InitializeComponent();
